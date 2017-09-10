7458c1cd4b5e4b5b8fe714e37af8851802413c23
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-09-10 11:06:44 +0200

    Extent program with a game model

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,32 @@
 module Main exposing (..)
 
 import Html
+import Game.Model as Game
 
 
-main : Html.Html msg
+main : Program Never Model msg
 main =
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    Game.Model
+
+
+model : Model
+model =
+    Game.create [ 2, 3, 4 ]
+
+
+view : Model -> Html.Html msg
+view _ =
     Html.text "Hello, World!"
+
+
+update : msg -> Model -> Model
+update _ model =
+    model
