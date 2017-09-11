184cf77bc9e1da9c567f6b9aa84a7b0887cf390e
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2017-09-11 08:48:33 +0200

    Create a data-position element to initialize game

--- docs/js/application.js
+++ docs/js/application.js
@@ -3,6 +3,17 @@
     containers = Array.prototype.slice.call(containers);
     containers.forEach(function(container){
         var app = Elm.Main.embed(container);
-        app.ports.reset.send([3, 4, 5]);
+        var position = container.dataset.position;
+        if (position) {
+            position = parseList(position);
+            app.ports.reset.send(position);
+        }
     });
+
+    function parseList(input){
+        return input
+            .trim()
+            .split(/\s*,\s*/)
+            .map(function(element){ return parseInt(element, 10); });
+    }
 })(Elm);
